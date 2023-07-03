66a6e6dce4e5ee7626a5b5d254e844c4f5c0905c
lx5555 <71442208+lx5555@users.noreply.github.com>
2023-07-03 10:35:45 +0800

    Update stream-start.sh

--- stream-start.sh
+++ stream-start.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
 
-docker cp ./austin-stream/target/austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/
-docker exec -ti austin_jobmanager_1 flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
